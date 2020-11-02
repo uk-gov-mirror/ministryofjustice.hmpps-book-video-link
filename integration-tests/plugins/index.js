d232f3b82ba0b8ddb2753040f4c46f67d59ceea0
Louise N <lnorris47@gmail.com>
2020-11-02 16:27:08 +0000

    DCS-654 removed unused code from prisonAPI and elite2 (#39)
    Co-authored-by: Andrew Lee <1517745+andrewrlee@users.noreply.github.com>

--- integration-tests/plugins/index.js
+++ integration-tests/plugins/index.js
@@ -2,21 +2,8 @@ const auth = require('../mockApis/auth')
 const prisonApi = require('../mockApis/prisonApi')
 const whereabouts = require('../mockApis/whereabouts')
 const tokenverification = require('../mockApis/tokenverification')
-const activityResponse = require('../mockApis/responses/activityResponse')
-const {
-  courtEventsWithDifferentStatusResponse,
-  externalTransfersResponse,
-} = require('../mockApis/responses/houseBlockResponse')
-const alertsResponse = require('../mockApis/responses/alertsResponse')
 
 const { resetStubs } = require('../mockApis/wiremock')
-
-const extractOffenderNumbers = activityList => {
-  const result = Object.keys(activityList).reduce((r, k) => {
-    return r.concat(activityList[k])
-  }, [])
-  return [...new Set(result.map(item => item.offenderNo))]
-}
 
 module.exports = on => {
   on('task', {
@@ -25,10 +12,10 @@ module.exports = on => {
       await resetStubs()
       return tokenverification.stubVerifyToken(true)
     },
-    stubAuthHealth: status => Promise.all([auth.stubHealth(status)]),
-    stubPrisonApiHealth: status => Promise.all([prisonApi.stubHealth(status)]),
-    stubWhereaboutsHealth: status => Promise.all([whereabouts.stubHealth(status)]),
-    stubTokenverificationHealth: status => Promise.all([tokenverification.stubHealth(status)]),
+    stubAuthHealth: status => auth.stubHealth(status),
+    stubPrisonApiHealth: status => prisonApi.stubHealth(status),
+    stubWhereaboutsHealth: status => whereabouts.stubHealth(status),
+    stubTokenverificationHealth: status => tokenverification.stubHealth(status),
 
     stubHealthAllHealthy: () =>
       Promise.all([
@@ -48,186 +35,29 @@ module.exports = on => {
     stubLoginCourt: () =>
       Promise.all([auth.stubLoginCourt(), prisonApi.stubUserCaseloads(), tokenverification.stubVerifyToken(true)]),
 
-    stubUserEmail: username => Promise.all([auth.stubEmail(username)]),
-    stubUser: (username, caseload) => Promise.all([auth.stubUser(username, caseload)]),
-    stubStaff: ({ staffId, details }) => Promise.all([prisonApi.stubStaff(staffId, details)]),
-    stubScheduledActivities: response => Promise.all([prisonApi.stubUserScheduledActivities(response)]),
-    stubProgEventsAtLocation: ({ caseload, locationId, timeSlot, date, activities }) =>
-      Promise.all([prisonApi.stubProgEventsAtLocation(caseload, locationId, timeSlot, date, activities)]),
-
-    stubCourts: courts => Promise.all([whereabouts.stubCourtLocations(courts)]),
+    stubUserEmail: username => auth.stubEmail(username),
+    stubUser: (username, caseload) => auth.stubUser(username, caseload),
+    stubCourts: courts => whereabouts.stubCourtLocations(courts),
     stubGroups: caseload => whereabouts.stubGroups(caseload),
-    stubAddVideoLinkAppointment: appointment => Promise.all([whereabouts.stubAddVideoLinkAppointment(appointment)]),
+    stubAddVideoLinkAppointment: appointment => whereabouts.stubAddVideoLinkAppointment(appointment),
 
-    stubForAttendance: ({ caseload, locationId, timeSlot, date, activities }) => {
-      const offenderNumbers = extractOffenderNumbers(activities)
-      return Promise.all([
-        prisonApi.stubUserCaseloads(),
-        prisonApi.stubProgEventsAtLocation(locationId, timeSlot, date, activities),
-        prisonApi.stubUsageAtLocation(caseload, locationId, timeSlot, date, 'APP'),
-        prisonApi.stubUsageAtLocation(caseload, locationId, timeSlot, date, 'VISIT'),
-        prisonApi.stubVisits(activityResponse.visits),
-        prisonApi.stubActivityLocations(),
-        prisonApi.stubAppointments(),
-        prisonApi.stubActivities(),
-        prisonApi.stubCourtEvents(),
-        prisonApi.stubExternalTransfers(),
-        prisonApi.stubAlerts({ locationId: 'MDI', alerts: alertsResponse }),
-        prisonApi.stubAssessments(offenderNumbers),
-        prisonApi.stubOffenderSentences(offenderNumbers, date),
-      ])
-    },
-
-    stubGetActivityList: ({ caseload, locationId, timeSlot, date, inThePast = false, activities }) => {
-      let activity
-      if (activities) {
-        activity = activities
-      } else {
-        activity = inThePast ? activityResponse.pastActivities : activityResponse.activities
-      }
-      const offenderNumbers = extractOffenderNumbers(activity)
-      return Promise.all([
-        prisonApi.stubUserCaseloads(),
-        prisonApi.stubProgEventsAtLocation(locationId, timeSlot, date, activity),
-        prisonApi.stubUsageAtLocation(caseload, locationId, timeSlot, date, 'APP'),
-        prisonApi.stubUsageAtLocation(caseload, locationId, timeSlot, date, 'VISIT'),
-        prisonApi.stubVisits(activityResponse.visits),
-        prisonApi.stubActivityLocations(),
-        prisonApi.stubAppointments(activityResponse.appointments),
-        prisonApi.stubActivities(activityResponse.activities),
-        prisonApi.stubCourtEvents(courtEventsWithDifferentStatusResponse),
-        prisonApi.stubExternalTransfers(externalTransfersResponse),
-        prisonApi.stubAlerts({ locationId: 'MDI', alerts: alertsResponse }),
-        prisonApi.stubAssessments(offenderNumbers),
-        prisonApi.stubOffenderSentences(offenderNumbers, date),
-      ])
-    },
-
-    stubAlertTypes: () => Promise.all([prisonApi.stubAlertTypes()]),
-    stubAlertsForBooking: alerts => Promise.all([prisonApi.stubAlertsForBooking(alerts)]),
-    stubAlerts: prisonApi.stubAlerts,
-
-    stubInmates: prisonApi.stubInmates,
-    stubUserLocations: prisonApi.stubUserLocations,
-
-    stubReleaseDatesOffenderNo: releaseDates => Promise.all([prisonApi.stubPrisonerSentenceDetails(releaseDates)]),
     stubVerifyToken: (active = true) => tokenverification.stubVerifyToken(active),
     stubLoginPage: auth.redirect,
-    verifyPostAttendance: () => whereabouts.verifyPostAttendance(),
-    stubSentenceAdjustments: response => prisonApi.stubGetSentenceAdjustments(response),
-    stubMovementsBetween: prisonApi.stubMovementsBetween,
     stubOffenderBasicDetails: basicDetails => Promise.all([prisonApi.stubOffenderBasicDetails(basicDetails)]),
-    stubOffenderFullDetails: fullDetails => Promise.all([prisonApi.stubOffenderFullDetails(fullDetails)]),
     stubAppointmentTypes: types => Promise.all([prisonApi.stubAppointmentTypes(types)]),
-    stubAppointmentsAtAgency: (agency, locations) =>
-      Promise.all([prisonApi.stubUsageAtAgency(agency, 'APP', locations)]),
-    stubVisitsAtAgency: (agency, locations) => Promise.all([prisonApi.stubUsageAtAgency(agency, 'VISIT', locations)]),
     stubActivityLocations: status => prisonApi.stubActivityLocations(status),
-    stubPostAppointments: () => Promise.all([prisonApi.stubPostAppointments()]),
-    stubSchedules: ({ agency, location, date, appointments, visits, activities }) =>
-      Promise.all([
-        prisonApi.stubSchedulesAtAgency(agency, location, 'APP', date, appointments),
-        prisonApi.stubSchedulesAtLocation(location, 'APP', date, appointments),
-        prisonApi.stubSchedulesAtAgency(agency, location, 'VISIT', date, visits),
-        prisonApi.stubSchedulesAtLocation(location, 'VISIT', date, visits),
-        prisonApi.stubCourtEvents(),
-        prisonApi.stubActivitySchedules(location, date, activities),
-        prisonApi.stubVisits(visits),
-        prisonApi.stubExternalTransfers(),
-        prisonApi.stubAppointments(appointments),
-        prisonApi.stubActivities(activities),
-      ]),
-    stubSentenceData: details => prisonApi.stubSentenceData(details),
-    stubLocation: ({ locationId, locationData }) => Promise.all([prisonApi.stubLocation(locationId, locationData)]),
     stubAgencyDetails: ({ agencyId, details }) => Promise.all([prisonApi.stubAgencyDetails(agencyId, details)]),
     stubAppointmentLocations: ({ agency, locations }) =>
       Promise.all([prisonApi.stubAppointmentLocations(agency, locations)]),
-    stubBookingOffenders: offenders => Promise.all([prisonApi.stubBookingOffenders(offenders)]),
     stubAgencies: agencies => Promise.all([prisonApi.stubAgencies(agencies)]),
     stubAppointmentsAtAgencyLocation: ({ agency, location, date, appointments }) =>
       Promise.all([prisonApi.stubSchedulesAtAgency(agency, location, 'APP', date, appointments)]),
-    stubCourtCases: courtCases => prisonApi.stubCourtCases(courtCases),
-    stubOffenceHistory: offenceHistory => prisonApi.stubOffenceHistory(offenceHistory),
-    stubSentenceTerms: sentenceTerms => prisonApi.stubSentenceTerms(sentenceTerms),
-    stubClientCredentialsRequest: () => auth.stubClientCredentialsRequest(),
     stubUserMeRoles: roles => auth.stubUserMeRoles(roles),
     stubUserMe: () => auth.stubUserMe(),
-    stubVisitsWithVisitors: ({ visitsWithVisitors, offenderBasicDetails, visitTypes }) =>
-      Promise.all([
-        prisonApi.stubVisitsWithVisitors(visitsWithVisitors),
-        prisonApi.stubOffenderBasicDetails(offenderBasicDetails),
-        prisonApi.stubVisitTypes(visitTypes),
-      ]),
-    stubSchedule: ({ offenderBasicDetails, thisWeeksSchedule, nextWeeksSchedule }) =>
-      Promise.all([
-        prisonApi.stubOffenderBasicDetails(offenderBasicDetails),
-        prisonApi.stubScheduledEventsForThisWeek(thisWeeksSchedule),
-        prisonApi.stubScheduledEventsForNextWeek(nextWeeksSchedule),
-      ]),
     stubAppointmentsGet: prisonApi.stubAppointmentsGet,
     stubVideoLinkAppointments: whereabouts.stubVideoLinkAppointments,
-    stubCreateAlert: prisonApi.stubCreateAlert,
     stubPrisonApiGlobalSearch: prisonApi.stubPrisonApiGlobalSearch,
-    stubOffenderImage: prisonApi.stubOffenderImage,
-    stubOffenderMovements: prisonApi.stubOffenderMovements,
-    stubBookingDetails: details => prisonApi.stubBookingDetails(details),
-    stubBookingNonAssociations: response => prisonApi.stubBookingNonAssociations(response),
     stubUserCaseLoads: caseloads => prisonApi.stubUserCaseloads(caseloads),
-    stubUpdateCaseload: prisonApi.stubUpdateCaseload,
-    stubCellAttributes: prisonApi.stubCellAttributes,
-    stubMainOffence: offence => prisonApi.stubMainOffence(offence),
-    stubCsraAssessments: ({ offenderNumbers, assessments }) =>
-      prisonApi.stubCsraAssessments(offenderNumbers, assessments),
-    stubInmatesAtLocation: ({ inmates }) => prisonApi.stubInmatesAtLocation(inmates),
-    stubOffenderCellHistory: ({ history }) => prisonApi.stubOffenderCellHistory(history),
-    stubGetAlerts: ({ agencyId, alerts }) => prisonApi.stubGetAlerts({ agencyId, alerts }),
-    stubGetAlert: ({ bookingId, alertId, alert }) => prisonApi.stubGetAlert({ bookingId, alertId, alert }),
-    stubPutAlert: ({ bookingId, alertId, alert }) => prisonApi.stubPutAlert({ bookingId, alertId, alert }),
-    stubHistoryForLocation: locationHistory => prisonApi.stubHistoryForLocation(locationHistory),
-    stubAttributesForLocation: locationAttributes => prisonApi.stubAttributesForLocation(locationAttributes),
-    stubPrisonerDetail: ({ prisonerDetail, bookingId }) => prisonApi.stubPrisonerDetail(prisonerDetail, bookingId),
-    stubPrisonerFullDetail: ({ prisonerDetail, offenderNo, fullInfo }) =>
-      prisonApi.stubPrisonerFullDetail(prisonerDetail, offenderNo, fullInfo),
-    stubMoveToCell: () => prisonApi.stubMoveToCell(),
-    stubMoveToCellSwap: () => prisonApi.stubMoveToCellSwap(),
-    verifyMoveToCell: ({ bookingId, locationPrefix }) => prisonApi.verifyMoveToCell({ bookingId, locationPrefix }),
-    stubGetLocationPrefix: ({ agencyId, groupName, response }) =>
-      whereabouts.stubGetLocationPrefix({ agencyId, groupName, response }),
-    verifyMoveToCellSwap: ({ bookingId }) => prisonApi.verifyMoveToCellSwap({ bookingId }),
-    stubEstablishmentRollCount: ({ agencyId, assignedMovements, unassignedMovements, movements, enroute }) =>
-      Promise.all([
-        prisonApi.stubAssignedMovements(agencyId, assignedMovements),
-        prisonApi.stubUnassignedMovements(agencyId, unassignedMovements),
-        prisonApi.stubRollcountByType(agencyId, 'movements', movements),
-        prisonApi.stubRollcountByType(agencyId, 'enroute', enroute),
-      ]),
-    stubCourtEvents: courtEvents => prisonApi.stubCourtEvents(courtEvents),
-    stubGetEventsByLocationIds: ({ agencyId, date, timeSlot, response }) =>
-      prisonApi.stubGetEventsByLocationIds(agencyId, date, timeSlot, response),
-    stubExternalTransfers: response => prisonApi.stubExternalTransfers(response),
-    stubAssessments: offenderNumbers => prisonApi.stubAssessments(offenderNumbers),
     stubLocationGroups: locationGroups => whereabouts.stubLocationGroups(locationGroups),
-    stubActivityLocationsByDateAndPeriod: ({ locations, date, period, withFault }) =>
-      prisonApi.stubActivityLocationsByDateAndPeriod(locations, date, period, withFault),
-    stubActivityLocationsConnectionResetFault: () => prisonApi.stubActivityLocationsConnectionResetFault(),
-    stubGetAdjudicationDetails: adjudicationDetails => prisonApi.stubGetAdjudicationDetails(adjudicationDetails),
-    stubAdjudicationFindingTypes: types => prisonApi.stubAdjudicationFindingTypes(types),
-    stubAdjudications: ({ response, headers }) => prisonApi.stubAdjudications(response, headers),
-    verifyAdjudicationsHistory: ({ offenderNo, agencyId, finding, fromDate, toDate }) =>
-      prisonApi.verifyAdjudicationsHistory({ offenderNo, agencyId, finding, fromDate, toDate }),
-    resetAdjudicationsStub: () => prisonApi.resetAdjudicationsStub(),
-    stubIepSummaryForBooking: iepSummary => prisonApi.stubIepSummaryForBooking(iepSummary),
-    stubMovementsIn: ({ agencyId, fromDate, movements }) =>
-      prisonApi.stubMovementsIn({ agencyId, fromDate, movements }),
-    stubMovementsOut: ({ agencyId, fromDate, movements }) =>
-      prisonApi.stubMovementsOut({ agencyId, fromDate, movements }),
-    stubIepSummaryForBookingIds: prisonApi.stubIepSummaryForBookingIds,
-    stubSystemAlerts: prisonApi.stubSystemAlerts,
-    stubInReception: ({ agencyId, results }) => prisonApi.stubRollcountByType(agencyId, 'in-reception', results),
-    stubEnRoute: ({ agencyId, results }) => prisonApi.stubEnRoute(agencyId, results),
-    stubCurrentlyOut: ({ livingUnitId, movements }) => prisonApi.stubCurrentlyOut(livingUnitId, movements),
-    stubTotalCurrentlyOut: ({ agencyId, movements }) => prisonApi.stubTotalCurrentlyOut(agencyId, movements),
-    stubGetAgencyIepLevels: response => prisonApi.stubGetAgencyIepLevels(response),
-    stubChangeIepLevel: body => prisonApi.stubChangeIepLevel(body),
   })
 }
