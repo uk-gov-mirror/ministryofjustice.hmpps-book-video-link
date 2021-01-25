f06ae5b83d75c80cbdf75c50f21fc785c57b2828
GCheema <50441412+GurnankCheema@users.noreply.github.com>
2021-01-25 15:43:14 +0000

    DCS-738 added integration tests (#133)

--- integration-tests/integration/bookings/amendBooking/bookingNotAvailable.spec.js
+++ integration-tests/integration/bookings/amendBooking/bookingNotAvailable.spec.js
@@ -16,83 +16,9 @@ context('A user can amend a booking', () => {
     cy.task('stubUserEmail', 'ITAG_USER')
     cy.task('stubUser', 'ITAG_USER', 'WWI')
 
-    // Stub booking list
-    cy.task('stubCourts')
-    cy.task('stubOffenderBookings', [
-      { bookingId: 1, firstName: 'OFFENDER', lastName: 'ONE' },
-      { bookingId: 2, firstName: 'OFFENDER', lastName: 'TWO' },
-    ])
-
-    cy.task('stubGetVideoLinkBookings', {
-      agencyId: '.*?',
-      date: moment().format('yyyy-MM-DD'),
-      bookings: [],
-    })
-
     cy.task('stubAppointmentLocations', {
       agency: '.*?',
       locations: [],
-    })
-
-    cy.task('stubAppointmentLocations', {
-      agency: 'WWI',
-      locations: [
-        { locationId: 100, userDescription: 'Room 1', locationType: 'VIDE' },
-        { locationId: 110, userDescription: 'Room 2', locationType: 'VIDE' },
-        { locationId: 120, userDescription: 'Room 3', locationType: 'VIDE' },
-      ],
-    })
-
-    cy.task('stubRoomAvailability', {
-      pre: [{ locationId: 100, description: 'Room 1', locationType: 'VIDE' }],
-      main: [{ locationId: 110, description: 'Room 2', locationType: 'VIDE' }],
-      post: [{ locationId: 120, description: 'Room 3', locationType: 'VIDE' }],
-    })
-
-    cy.task('stubGetVideoLinkBookings', {
-      agencyId: 'WWI',
-      date: moment().format('yyyy-MM-DD'),
-      bookings: [
-        {
-          agencyId: 'WWI',
-          bookingId: 1,
-          comment: 'A comment',
-          court: 'Leeds',
-          videoLinkBookingId: 10,
-          pre: {
-            locationId: 100,
-            startTime: '2020-01-02T12:40:00',
-            endTime: '2020-01-02T13:00:00',
-          },
-          main: {
-            locationId: 110,
-            startTime: '2020-01-02T13:00:00',
-            endTime: '2020-01-02T13:30:00',
-          },
-          post: {
-            locationId: 120,
-            startTime: '2020-01-02T13:30:00',
-            endTime: '2020-01-02T13:50:00',
-          },
-        },
-        {
-          agencyId: 'WWI',
-          bookingId: 2,
-          comment: 'A comment',
-          court: 'Other court',
-          videoLinkBookingId: 11,
-          pre: {
-            locationId: 100,
-            startTime: '2020-01-02T14:40:00',
-            endTime: '2020-01-02T15:00:00',
-          },
-          main: {
-            locationId: 110,
-            startTime: '2020-01-02T15:00:00',
-            endTime: '2020-01-02T15:30:00',
-          },
-        },
-      ],
     })
 
     // Stub booking details
@@ -129,22 +55,10 @@ context('A user can amend a booking', () => {
       },
     })
 
-    cy.task('stubLocation', {
-      locationId: 110,
-      response: {
-        description: 'vcc room 1',
-        locationId: 1,
-      },
-    })
-
     cy.task('stubAgencyDetails', {
       agencyId: 'WWI',
       details: { agencyId: 'WWI', description: 'Wandsworth', agencyType: 'INST' },
     })
-
-    cy.task('stubDeleteVideoLinkBooking', 10)
-
-    cy.task('stubAgencies', [{ agencyId: 'WWI', description: 'HMP Wandsworth' }])
 
     cy.task('stubPrisonApiGlobalSearch', [
       {
@@ -158,9 +72,7 @@ context('A user can amend a booking', () => {
         pncNumber: '1/2345',
       },
     ])
-  })
 
-  it('A user successfully amends a booking', () => {
     cy.task('stubLoginCourt')
 
     cy.task('stubRoomAvailability', {
@@ -168,9 +80,10 @@ context('A user can amend a booking', () => {
       main: [],
       post: [],
     })
+  })
 
+  it('A user is not able to amend a booking as no rooms available', () => {
     const tomorrow = moment().add(1, 'days')
-
     const bookingDetailsPage = BookingDetailsPage.goTo(10, 'John Doe’s')
     bookingDetailsPage.changeDate().click()
 
@@ -189,5 +102,54 @@ context('A user can amend a booking', () => {
     videoLinkNotAvailablePage.date().contains(tomorrow.format('dddd D MMMM YYYY'))
     videoLinkNotAvailablePage.startTime().contains('10:55')
     videoLinkNotAvailablePage.endTime().contains('11:55')
+  })
+
+  it('Should redirect user to retry with alternative appointment and previous request should already be populated', () => {
+    const tomorrow = moment().add(1, 'days')
+    const bookingDetailsPage = BookingDetailsPage.goTo(10, 'John Doe’s')
+    bookingDetailsPage.changeDate().click()
+    const changeDateAndTimePage = ChangeDateAndTimePage.verifyOnPage()
+    changeDateAndTimePage.form.date().type(tomorrow.format('DD/MM/YYYY'))
+    changeDateAndTimePage.activeDate().click()
+    changeDateAndTimePage.form.startTimeHours().select('10')
+    changeDateAndTimePage.form.startTimeMinutes().select('55')
+    changeDateAndTimePage.form.endTimeHours().select('11')
+    changeDateAndTimePage.form.endTimeMinutes().select('55')
+    changeDateAndTimePage.form.preAppointmentRequiredYes().click()
+    changeDateAndTimePage.form.postAppointmentRequiredYes().click()
+    changeDateAndTimePage.form.continue().click()
+
+    const videoLinkNotAvailablePage = VideoLinkNotAvailablePage.verifyOnPage()
+    videoLinkNotAvailablePage.continue().click()
+
+    ChangeDateAndTimePage.verifyOnPage()
+    changeDateAndTimePage.form.date().should('have.value', tomorrow.format('DD/MM/YYYY'))
+    changeDateAndTimePage.form.startTimeHours().contains('10')
+    changeDateAndTimePage.form.startTimeMinutes().contains('55')
+    changeDateAndTimePage.form.endTimeHours().contains('11')
+    changeDateAndTimePage.form.startTimeMinutes().contains('55')
+    changeDateAndTimePage.form.preAppointmentRequiredYes().should('have.value', 'yes')
+    changeDateAndTimePage.form.postAppointmentRequiredYes().should('have.value', 'yes')
+  })
+
+  it('Should redirect to details page', () => {
+    const tomorrow = moment().add(1, 'days')
+    const bookingDetailsPage = BookingDetailsPage.goTo(10, 'John Doe’s')
+    bookingDetailsPage.changeDate().click()
+    const changeDateAndTimePage = ChangeDateAndTimePage.verifyOnPage()
+    changeDateAndTimePage.form.date().type(tomorrow.format('DD/MM/YYYY'))
+    changeDateAndTimePage.activeDate().click()
+    changeDateAndTimePage.form.startTimeHours().select('10')
+    changeDateAndTimePage.form.startTimeMinutes().select('55')
+    changeDateAndTimePage.form.endTimeHours().select('11')
+    changeDateAndTimePage.form.endTimeMinutes().select('55')
+    changeDateAndTimePage.form.preAppointmentRequiredYes().click()
+    changeDateAndTimePage.form.postAppointmentRequiredYes().click()
+    changeDateAndTimePage.form.continue().click()
+
+    const videoLinkNotAvailablePage = VideoLinkNotAvailablePage.verifyOnPage()
+    videoLinkNotAvailablePage.cancel().click()
+
+    BookingDetailsPage.verifyOnPage('John Doe’s')
   })
 })
